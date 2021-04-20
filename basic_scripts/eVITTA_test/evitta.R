6f3102e0ebb8a3bb7561956089eda8b08f4e5f1a
easygsea <easygsea@gmail.com>
2021-04-20 13:37:00 -0700

    Updated eVITTA test code

--- basic_scripts/eVITTA_test/evitta.R
+++ basic_scripts/eVITTA_test/evitta.R
@@ -10,9 +10,7 @@ setwd("/Users/jeancheng/Documents/cSurvival/basic_scripts/eVITTA_test")
 df_gene_gender <- readRDS("df_gene_gender.rds") 
 patients <- df_gene_gender$patient_id
 genes <- colnames(df_gene_gender)[-1]
-df_gene_gender <- transpose(df_gene_gender) %>% .[-1,] %>%
-  dplyr::mutate_all(as.numeric) %>%
-  as.matrix(.)
+df_gene_gender <- transpose(df_gene_gender) %>% .[-1,] %>% as.data.frame()
 # reassign patient ids
 colnames(df_gene_gender) <- patients
 rownames(df_gene_gender) <- genes
@@ -29,6 +27,27 @@ head(id_table)
 # extract the gene ids from df_gene and find their names from id conversion table
 gene_ids <- as_tibble_col(rownames(df_gene_gender), column_name = "ensembl_id")
 gene_ids_table <- left_join(gene_ids, id_table, by="ensembl_id")
+
+# convert gene ids
+df_gene_gender <- df_gene_gender %>% dplyr::mutate(ensembl_id=rownames(df_gene_gender)) %>%
+  left_join(gene_ids_table, by = "ensembl_id")
+
+# remove unnecessary columns
+df_gene_gender <- df_gene_gender %>%
+  dplyr::distinct(symbol,.keep_all = TRUE) %>%
+  dplyr::filter(!is.na(symbol))
+
+genes <- df_gene_gender$symbol
+rownames(df_gene_gender) <- genes
+
+# convert to numeric matrix
+df_gene_gender <- df_gene_gender %>%
+  dplyr::select(-c(ensembl_id, gene_id, symbol)) %>%
+  dplyr::mutate_all(as.numeric) %>%
+  as.matrix(.)
+
+# resave data
+saveRDS(df_gene_gender,"df_gene_gender.rds") 
 
 # # retrieve design data
 df_design_gender <- readRDS("df_design_gender.rds")
@@ -55,19 +74,23 @@ fit <- eBayes(fit,trend=TRUE, robust=TRUE)
 results <- decideTests(fit)
 summary(results)
 
+# available coefficients
+coefs <- colnames(design)[-1]
+
+# name the coefficients
+lels1 <- levels(df_design_gender$level.x) %>% rev()
+lels2 <- levels(df_design_gender$level.y) %>% rev()
+names(coefs) <- c(
+  paste0(lels1, collapse = " vs. "),
+  paste0(lels2, collapse = " vs. "),
+  paste0("(",paste0(lels2, collapse = " vs. "),") vs. (",paste0(lels1, collapse = " vs. "),")")
+)
+
 # export DEG table
-degs = topTable(fit, coef=1,sort.by="P",number=Inf)
+degss = lapply(coefs, function(x){
+  topTable(fit, coef=x,sort.by="P",number=Inf)
+})
+names(degss) <- coefs
 
-degs <- degs %>% mutate(ensembl_id=rownames(degs)) %>%
-  left_join(gene_ids_table, by = "ensembl_id") %>%
-  dplyr::distinct(symbol,.keep_all = TRUE) %>%
-  dplyr::filter(!is.na(symbol))
-
-head(degs)
-
-# the genes
-genes <- degs$symbol
-
-# reformat the table
-degs <- degs %>% dplyr::select(-c(ensembl_id, gene_id, symbol))
-rownames(degs) <- genes
+saveRDS(degss,"degss.rds")
+saveRDS(coefs,"coefs.rds")
