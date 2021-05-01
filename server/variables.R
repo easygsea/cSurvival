d0d0caf3b0702c74b1a272c285803a2fbb8bd470
easygsea <easygsea@gmail.com>
2021-05-01 10:32:52 -0700

    GMT variables moved into server

--- server/variables.R
+++ server/variables.R
@@ -1,5 +1,35 @@
+# directory to temporarily save variables
 surv_dir <- paste0(dirname(dirname(getwd())), "/variables/")
 
+# ------ available GMT gene sets -----
+# gmt_dir <- paste0(dirname(getwd()),"/eVITTA_dev/easyGSEA/www/gmts/hsa/")
+gmt_dir <- "http://tau.cmmt.ubc.ca/eVITTA/easyGSEA/gmts/hsa/"
+# # collection in a df
+# in_gmt_lst <- paste0(dirname(getwd()),"/eVITTA_dev/easyGSEA/www/gmts/gmts_list.csv") # code test locally
+in_gmt_lst <- "http://tau.cmmt.ubc.ca/eVITTA/easyGSEA/gmts/gmts_list.csv"
+gmt_collection_df <- read_csv(in_gmt_lst,col_names = F) %>% dplyr::filter(X1 == "hsa")
+# available databases
+gmt_dbs <- str_split(gmt_collection_df$X3,";") %>% lapply(function(x) gsub("_"," ",gsub("?[.]gmt$","",x)))
+db_names <- gsub("_"," ",gsub("^\\d+_?","",gmt_collection_df$X2))
+names(gmt_dbs) <- db_names
+# function to find the full path to a selected GMT
+retrieve_gmt_path <- function(db){
+  db <- paste0(gsub(" ","_",db),".gmt")
+  # determine which subdirectory the library is in
+  i <- grep(db, gmt_collection_df$X3)
+  sub_dir <- gmt_collection_df$X2[i]
+  paste0(gmt_dir,sub_dir,"/",db) %>% gsub(" ","%20",.)
+}
+# gmt_files <- list.files(gmt_dir, pattern = "[.]gmt$", recursive = T, full.names = T) %>% .[!grepl("TF2DNA[.]gmt$",.)]
+# gmts <- gmt_files %>%
+#   lapply(., function(x){
+#     # cat_name <- basename(dirname(x)) %>% gsub("^\\d+_?","",.)
+#     # db_name <- gsub("?[.]gmt$","",basename(x)) %>% as.list()
+#     # names(db_name) <- cat_name
+#     # return(db_name)
+#     gmtPathways(x)
+#   })
+# names(gmts) <- gsub("?[.]gmt$","",basename(gmt_files))
 # ------- names for input modes ----------
 input_mode_names <- c(
   "Expression (FPKM)" = "rna"
